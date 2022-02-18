14c3fb2b70e917ad99cebfa0d584783130e63a9f
Guillaume Tremeau <tremeauguillaume@gmail.com>
2022-02-18 20:35:58 +0100

    init

---
+++ app/root.jsx
@@ -0,0 +1,39 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+
+export function meta() {
+  return { title: "Guillaume Tremeau" };
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.jsx
@@ -0,0 +1,74 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Link,
+  Typography,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        mt={3}
+      >
+        <Avatar
+          alt="Guillaume Tremeau"
+          src="/guillaume.jpg"
+          sx={{ width: 76, height: 76 }}
+          mb={2}
+        />
+        <Typography variant="h5" align="center" mb={2}>
+          Code with Guillaume
+        </Typography>
+        <Typography align="center" mb={2}>
+          Hi! I'm Guillaume Tremeau, a freelance web developer and passionate
+          photographer.
+        </Typography>
+        <Box
+          sx={{
+            typography: "body1",
+            "& > :not(style) + :not(style)": {
+              ml: 2,
+            },
+          }}
+          mb={2}
+        >
+          <Link
+            href="https://www.linkedin.com/in/guillaumetremeau/"
+            underline="none"
+          >
+            <LinkedInIcon />
+          </Link>
+          <Link href="https://github.com/guillaumetremeau" underline="none">
+            <GitHubIcon />
+          </Link>
+        </Box>
+        <Button
+          variant="outlined"
+          href="https://guillaumetremeau.com"
+          fullWidth
+        >
+          Check my photos!
+        </Button>
+        <Box mt={3} typography="subtitle2">
+          <Link underline="none" href="tel:+330770818649">
+            +33 (0) 7 70 81 86 49
+          </Link>
+        </Box>
+        <Box mt={1} typography="subtitle2">
+          <Link underline="none" href="mailto:tremeauguillaume@gmail.com">
+            tremeauguillaume@gmail.com
+          </Link>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
